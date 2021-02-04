8f39875cd40c6936a821aea9aed1c8d7d2c8882b
kurokokoruru <kurokokoruru@gmail.com>
2021-02-04 18:14:02 +0900

    headerのデザインをしてみる

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        ヘッダーです。
+  <nav class="flex items-center justify-between flex-wrap bg-green-300 p-6">
+    <div class="flex items-center flex-no-shrink mr-6">
+      <img
+        class="h-8 w-8 mr-2"
+        width="54"
+        height="54"
+        viewBox="0 0 54 54"
+        src="@/assets/images/logo.png"
+      />
+      <span class="font-semibold text-xl tracking-tight text-gray-600">stubooo</span>
     </div>
+    <div class="w-auto flex-grow flex items-center">
+      <div class="text-sm flex-grow">
+        <a
+          href="#responsive-header"
+          class="inline-block mt-0 text-gray-600 hover:text-white mr-4"
+        >
+          Rest API
+        </a>
+        <a
+          href="#responsive-header"
+          class="inline-block mt-0 text-gray-600 hover:text-white mr-4"
+        >
+          websocket
+        </a>
+        <a
+          href="#responsive-header"
+          class="inline-block mt-0 text-gray-600 hover:text-white"
+        >
+          Help
+        </a>
+      </div>
+    </div>
+  </nav>
 </template>
